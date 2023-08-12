25268dc8438a8441ca69e1a9c63b21346b33c98d
afadza <arya.skoba@gmail.com>
2023-08-12 11:01:53 +0700

    day-5

--- js/my-blog.js
+++ js/my-blog.js
@@ -8,6 +8,24 @@ function addBlog(event) {
   let image = document.getElementById("input-blog-image").files;
   let dateStart = document.getElementById("date-input-start").value;
   let dateEnd = document.getElementById("date-input-end").value;
+  let durationStart = new Date(
+    document.getElementById("date-input-start").value
+  );
+  let durationEnd = new Date(document.getElementById("date-input-end").value);
+
+  let timeDiff = Math.abs(durationStart - durationEnd); // Match.abs() berfungsi untuk merubah format yang tadi minus atau negatif menjadi positif, jadi pada kasus ini isi dari Match.abs() adalah pengurangan antara dateEnd - dateStart.
+  let selisihHari = Math.ceil(timeDiff / (1000 * 60 * 60 * 24)); // Match.ceil berfungsi untuk membulatkan bilangan keatas, jadi pada kasus ini Match.ceil() berfungsi untuk membagi hasil pengurangan dari timeDiff dibagi 1 hari.
+
+  let hasil; // Ini adalah variable kosong yang akan diisi sesuai kondisi if else dibawah
+
+  if (selisihHari >= 30 && selisihHari < 365) {
+    // jika selisihHari lebih atau sama dengan dari 30
+    hasil = Math.floor(selisihHari / 30) + " bulan"; // maka variable hasil berisi 30 dibagi 30 dan hasilnya adalah 1
+  } else if (selisihHari >= 365) {
+    hasil = Math.floor(selisihHari / 365) + " tahun";
+  } else {
+    hasil = selisihHari + " hari"; // tapi jika selisihHari kurang dari 30 maka variable hasil isinya adalah ini
+  }
 
   image = URL.createObjectURL(image[0]);
   console.log(image);
@@ -16,6 +34,8 @@ function addBlog(event) {
     title,
     dateStart,
     dateEnd,
+    hasil,
+    postAt: new Date(),
     content,
     image,
   };
@@ -36,11 +56,17 @@ function renderBlog() {
     <div class="blog-list-item">
     <div class="blog-image">
       <img src="${dataBlog[index].image}" alt="" />
+      <p style="font-size: 10px; color: grey">Diposting pada ${getFullTime(
+        dataBlog[index].postAt
+      )}</p>
     </div>
     <h1>
       <a href="blog-detail.html" target="_blank">${dataBlog[index].title}</a>
     </h1>
-    <div class="detail-blog-content">${dataBlog[index].dateStart} | ${dataBlog[index].dateEnd}</div>
+    <div class="detail-blog-content">${dataBlog[index].dateStart} sampai ${
+      dataBlog[index].dateEnd
+    }</div>
+    <p style="margin-bottom: 20px;">Durasi : ${dataBlog[index].hasil}</p>
     <p>
     ${dataBlog[index].content}
     </p>
@@ -56,4 +82,51 @@ function renderBlog() {
   </div>
     `;
   }
+}
+
+function getFullTime(time) {
+  // new Date() mendapatkan terkait tanggal dan waktu kapan fungsinya dijalankan
+  // let time = new Date();
+  // console.log(time);
+
+  let monthName = [
+    "Jan",
+    "Feb",
+    "Mar",
+    "Apr",
+    "May",
+    "Jun",
+    "Jul",
+    "Aug",
+    "Sep",
+    "Oct",
+    "Nov",
+    "Dec",
+  ];
+  // console.log(monthName[0]);
+
+  let date = time.getDate();
+  // console.log(date);
+
+  let monthIndex = time.getMonth();
+  // console.log(monthIndex);
+  // console.log(monthName[monthIndex]);
+
+  let year = time.getFullYear();
+  // console.log(year);
+
+  let hours = time.getHours();
+  let minutes = time.getMinutes();
+
+  if (hours <= 9) {
+    // 09
+    hours = "0" + hours;
+  } else if (minutes <= 9) {
+    minutes = "0" + minutes;
+  }
+
+  // console.log(`${hours}:${minutes}`);
+
+  // 11 Aug 2023 09:18 WIB
+  return `${date} ${monthName[monthIndex]} ${year} ${hours}:${minutes} WIB`;
 }
